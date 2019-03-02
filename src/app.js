be1f33047400e4f2c6b0e7e87a694992fcabb96d
Karol <karolcichonski@wp.pl>
2019-03-02 08:21:06 +0100

    Step 3_1: New features- input field validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import './assets/scss/app.scss';
-import $ from 'cash-dom';
+import $, { each } from 'cash-dom';
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
@@ -7,19 +7,39 @@ require('isomorphic-fetch');
 export class App {
   initializeApp() {
     const self = this;
+    let nameFlag=false;
+
+    $('.username').on('change', function (e) {
+      self.checkInputTex();
+    });
 
     $('.load-username').on('click', function (e) {
+      nameFlag=self.checkInputTex();
       let userName = $('.username.input').val();
-
-      fetch(`https://api.github.com/users/${userName}`)
-        .then(response => response.json())
-        .then(response => {
-          self.profile = response;
-          self.update_profile();
-        })
-
+      if (nameFlag && userName.length != 0){
+        fetch(`https://api.github.com/users/${userName}`)
+          .then(response => response.json())
+          .then(response => {
+            self.profile = response;
+            self.update_profile();
+          })
+      }else{
+        $('.username').addClass('redBorder');
+        nameFlag = false;
+      }
     })
+  }
 
+  checkInputTex(){
+    const userName = $('.username.input').val();
+    const reg = /[^a-z0-9_-]/;
+    if (reg.test(userName)) {
+      $('.username').addClass('redBorder');
+      return false;
+    } else {
+      $('.username').removeClass('redBorder');
+      return true;
+    }
   }
 
   update_profile() {
@@ -29,3 +49,4 @@ export class App {
     $('#profile-bio').text(this.profile.bio || '(no information)');
   }
 }
+
